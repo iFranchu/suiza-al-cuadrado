7fb3a5222fde940fcd29e42d2d141ee7abbba8ea
FranciscoTrujillo <trujilloszitai@gmail.com>
2021-10-28 15:35:43 -0300

    Se ha avanzado parcialmente con el analisis de funciones lineales

--- Tarea006/src/js/funcion-lineal.js
+++ Tarea006/src/js/funcion-lineal.js
@@ -1,5 +1,7 @@
 const btnAlternarCalculadora = document.querySelectorAll(".goToFunction"), calculadora = document.querySelector(".calculadora"), contFuncion = document.querySelector(".contenedor-funcion");
 
+let infoAbscisas = document.getElementById("info-abscisas"), infoOrdenada = document.getElementById("info-ordenada"), direccionFuncion = document.getElementById("direccion-funcion"), intervaloPositivo = document.getElementById("info-intervalo+"), intervaloNegativo = document.getElementById("info-intervalo-"); 
+
 btnAlternarCalculadora.forEach(btn => {
     btn.addEventListener("click", ()=>{
         calculadora.classList.toggle("invisible");
